4d411886453621d0db13feed8d165ed25d943d91
Jae Lee <jael@stanford.edu>
2020-09-30 16:26:50 -0700

    Cleanup workflow around expiration

--- src/landing.php
+++ src/landing.php
@@ -10,7 +10,7 @@ use REDCap;
 $module->emDebug("Starting CROP landing page for project $pid");
 
 $sunet_id = $_SERVER['WEBAUTH_USER'];
-$sunet_id = 'soda';
+$sunet_id = 'test';//'soda';
 
 //if sunet ID not set leave
 if (!isset($sunet_id)) {
